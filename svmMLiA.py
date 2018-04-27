02cc0df6871b50ffe7203cdd31fa2fbb5ecea4fb
shuiqukeyou <19941212d>
2018-04-28 00:26:43 +0800

    完整Platt算法

---
+++ svmMLiA.py
@@ -0,0 +1,186 @@
+from numpy import *
+
+# 读取数据
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    # 返回数据矩阵和标签矩阵
+    return dataMat, labelMat
+
+
+#
+def selectJrand(i, m):
+    j = i
+    while (j == i):
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj,H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
+# 简化版SOM
+# 数据集、标签集、常数C、容错率、最大迭代次数
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    # 数据集和标签集转置
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    # 公式中用到的一个重要常数项
+    b = 0
+    # 行列数
+    m,n = shape(dataMatrix)
+    # 创建初始alphas，初始值为m行的单列矩阵
+    alphas = mat(zeros((m, 1)))
+    # 迭代次数
+    iter = 0
+    # 只要迭代次数少于设定的迭代上限，就继续进行遍历
+    while (iter < maxIter):
+        # 每轮迭代前将标记值记0，0表示alpha尚未进行优化
+        alphaPairsChanged = 0
+        for i in range(m):
+            # 预测类别
+            # multiply为矩阵各项相乘（不是矩阵乘法），获得由alphas处理过后的标签矩阵，在对其进行转置
+            # dataMatrix * dataMatrix[i, : ].T：用数据矩阵 X 当前行的转置
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, : ].T)) + b
+            # 计算预测的类别和实际值的差值
+            Ei = fXi - float(labelMat[i])
+            # 如果对应标签 * 预测值得绝对值大于容错率（分别对应正容差和负容差）
+            # 并且对应情况的alphas[i]没有超过传入的常数C，则可以进行优化
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                # 由selectJrand随机选择i~m间的一个值将其和i组成值对
+                j = selectJrand(i ,m)
+                # 同样计算j的预测类别
+                fXj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T)) + b
+                # 计算j的预测值的误差
+                Ej = fXj - float(labelMat[j])
+                # 为了避免多重引用，将alphas中对应的i和j值备份
+                alphaIold = alphas[i].copy()
+                alphaJold = alphas[j].copy()
+                # 计算L和H，i和j对应的标签相同和不相同时需要分别处理
+                # L和H为这个数据对和分隔超平面的距离
+                if (labelMat[i] != labelMat[j]):
+                    # 若不为同类，则这两个点应该各自在分隔超平面的两侧
+                    L = max(0, alphas[j] - alphas[i])
+                    H = min(C, C + alphas[j] - alphas[i])
+                else:
+                    # 若为同类，则这两个点应该各自在分隔超平面的同侧
+                    L = max(0, alphas[j] + alphas[i] - C)
+                    H = min(C, alphas[j] + alphas[i])
+                # 如果计算出的L和H相等，则表明分隔超平面正好在中间，终止本次循环
+                if L == H:
+                    print('L == H')
+                    continue
+                # 计算alpha[j]的最优修改量
+                eta = 2 * dataMatrix[i, : ] * dataMatrix[j, : ].T - dataMatrix[i, : ] * dataMatrix[i, : ].T - dataMatrix[j, : ] * dataMatrix[j, : ].T
+                # 简化部分
+                # 实际上当eta值为0时，需要退出这轮for循环
+                if eta >= 0:
+                    print('eta>=0')
+                    continue
+                # 计算新alphas[j]值
+                alphas[j] -= labelMat[j] * (Ei - Ej)/eta
+                alphas[j] = clipAlpha(alphas[j], H, L)
+                # 当alphas[j]和旧alphas[j]值差值极小时，直接直接终止本次循环，不对alphas[i]值进行调整
+                if (abs(alphas[j] - alphaJold) < 0.00001):
+                    print('alphas[j]移动不够充分')
+                    continue
+                # 对alphas[i]进行同值但反向的调整
+                alphas[i] += labelMat[j] * labelMat[i] * (alphaJold - alphas[j])
+                # 更新常数项
+                b1 = b - Ei - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, : ] * dataMatrix[i, : ].T - labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[i, : ] * dataMatrix[j, : ].T
+                b2 = b - Ej - labelMat[i] * (alphas[i] - alphaIold) * dataMatrix[i, : ] * dataMatrix[j, : ].T - labelMat[j] * (alphas[j] - alphaJold) * dataMatrix[j, : ] * dataMatrix[j, : ].T
+                if (0 < alphas[i]) and (C > alphas[i]):
+                    b = b1
+                elif (0 < alphas[j]) and (C > alphas[j]):
+                    b = b2
+                else:
+                    b = (b1 + b2)/2
+                # 成功的改变了一对alpha，变化标记+1
+                alphaPairsChanged += 1
+                print('第%d次迭代  在第%d项时，当前数据对改变%d次'%(iter, i, alphaPairsChanged))
+        # 退出上方循环后判断是否完成了一轮迭代
+        # 如果标记值没有变动，则完成了一次收敛，迭代次数+1
+        if (alphaPairsChanged == 0 ):
+            iter += 1
+        else:
+            iter = 0
+        print('迭代次数: %d' % iter)
+    return b, alphas
+
+
+# 实质上是利用这个类做一个数据结构
+# 对应简化版SOM函数中的各项重要参数的初始化
+class optStruct(object):
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.tol = toler
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        # 误差缓存
+        self.eCache = mat(zeros((self.m, 2)))
+
+
+# 计算给定值的E值
+def calcEk(oS, k):
+    fXk = float(multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, : ])) + oS.b
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+
+# 计算J的最佳改变值
+def selectJ(i, oS, Ei):
+    maxK = -1
+    maxDeltaE = 0
+    Ej = 0
+    oS.eCeche[i] = [1, Ei]
+    # 返回非0E值对应的alpha值
+    validEcacheList = nonzero(oS.eCeche[ : , ].A)[0]
+    # 若有多个可用的alpha值，则进行遍历
+    if len(validEcacheList) > 1:
+        # 遍历，选出使得改变最大的值
+        for k in validEcacheList:
+            if k == i:
+                continue
+            Ek = calcEk(Ei - Ek)
+            deltaE = abs(Ei - Ek)
+            if (deltaE > maxDeltaE):
+                maxK = k
+                maxDeltaE = deltaE
+                Ej = Ek
+        return maxK, Ej
+    else:
+        # 随机选择一个
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
+
+# 计算误差并存入缓存
+def updateEk(oS, k):
+    Ek = calcEk(oS, k)
+    oS.eCeche[k] = [1, Ek]
+
+
+def innerL(i, oS):
+    pass
+
+
+if __name__ == '__main__':
+    dataArr, labelArr = loadDataSet('./data/ch06/testSet.txt')
+    # print(labelArr)
+    b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
+    print(b)
+    print(alphas[alphas>0])
+
